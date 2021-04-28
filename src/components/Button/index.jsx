a7f5cb713dfca1f7a9fbe671c956d955eb5fb550
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2021-04-28 10:33:15 -0300

    Classe button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export class Button extends Component {
+    render() {
+        const {text, onClick} = this.props;
+
+        return (
+            <button className="button" onClick={onClick}>
+                {text}
+            </button>
+        );
+    }
+}
