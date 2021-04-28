925b49ffdb53eb6ef51b28a53fce131eb87db349
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2021-04-28 10:32:32 -0300

    Atualização do index.jsx

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -3,11 +3,14 @@ import { Component } from 'react';
 
 import { loadPosts } from '../../utils/load-posts';
 import { Posts } from '../../components/Posts';
+import { Button } from '../../components/Button';
 
 export class Home extends Component {
   state = {    
     posts: [],
     allPosts: [],
+    page: 0,
+    postsPerPage: 10
   };
 
   async componentDidMount(){
@@ -15,16 +18,41 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
+
     const postsAndPhotos = await loadPosts();
-    this.setState({ posts: postsAndPhotos });    
+    this.setState({ 
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });    
   }
+
+  loadMorePosts = () => {
+    const {
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage});
+  }
+
 
   render() {
     const { posts } = this.state;
 
     return (
       <section className="container">
-        <Posts posts={posts} />        
+        <Posts posts={posts} />  
+        <div className="button-container">
+          <Button
+          text="Load more posts"
+          onClick={this.loadMorePosts} />              
+         </div>
       </section>
     );
   }
